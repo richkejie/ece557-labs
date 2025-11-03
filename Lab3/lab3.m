51d4af9f724e32904825902ceaacdc647f1e5bc5
RichardMac <richkejie@gmail.com>
2025-11-02 23:28:52 -0500

    update lab3.m...not working...

--- Lab3/lab3.m
+++ Lab3/lab3.m
@@ -1,4 +1,5 @@
 %% 4.2
+%% constants
 M = 1.1911;
 m = 0.2300;
 l = 0.3302;
@@ -6,6 +7,7 @@ g = 9.8;
 alpha_1 = 1.7265;
 alpha_2 = 10.5;
 
+%% symbolic vars and state equations
 syms z z_dot theta theta_dot u real;
 
 z_ddot = (-m*l*sin(theta)*theta_dot^2 + m*g*sin(theta)*cos(theta) + alpha_1*u - alpha_2*z_dot) / (M + m*(sin(theta))^2);
@@ -16,8 +18,9 @@ f = [z_dot;z_ddot;theta_dot;theta_ddot]
 
 matlabFunction(eval(f), 'Vars', {x,u}, 'File', 'cartpend', 'Outputs', {'xdot'});
 
-cartpend([0 0 pi/2 0]',1)
+cartpend_test = cartpend([0;0;pi/2;0],1)
 
+%% jacobians
 J1 = jacobian(f,x);
 J2 = jacobian(f,u);
 
@@ -25,6 +28,17 @@ J2 = jacobian(f,u);
 x_bar = [0;0;0;0];
 u_bar = 0;
 
-A = subs(J1, [x;u], [x_bar;u_bar]);
-B = subs(J2, [x;u], [x_bar;u_bar]);
+A = subs(J1, [x;u], [x_bar;u_bar])
+B = subs(J2, [x;u], [x_bar;u_bar])
+
+%% controllability
+ctrl_AB = ctrb(A,B)
+rankA = rank(A)
+rankB = rank(B)
+
+%% 4.3
+p = [-5,-5,-5,-5];
+K_place = acker(A,B,p)
+eig(A+B*K+place)
+
 
